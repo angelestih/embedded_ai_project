a22b6fefec204ced948edebf11dd2a753fcb236d
Angel Daniel Estigarribia Hobecker <71296981+angelestih@users.noreply.github.com>
2022-04-30 16:19:13 -0700

    Update person_detection_neural.py

--- tensorflow/person_detection_neural.py
+++ tensorflow/person_detection_neural.py
@@ -93,7 +93,7 @@ print("Data set randomization and splitting complete.")
 # build the model and train it
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Dense(8, activation='relu')) 
-model.add(tf.keras.layers.Dense(5, activation='relu'))
+model.add(tf.keras.layers.Dense(8, activation='relu'))
 model.add(tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'))
 model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
 history = model.fit(inputs_train, outputs_train, epochs=400, batch_size=4, validation_data=(inputs_validate, outputs_validate))
