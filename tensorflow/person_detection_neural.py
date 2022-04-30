7000d7e68bfd685641047176f003712b39e8ac9f
Angel Daniel Estigarribia Hobecker <71296981+angelestih@users.noreply.github.com>
2022-04-30 13:59:30 -0700

    Added files

---
+++ tensorflow/person_detection_neural.py
@@ -0,0 +1,142 @@
+﻿!sudo apt-get update -y
+!sudo apt-get install -y xxd
+!pip install pandas numpy matplotlib
+%tensorflow_version 2.x
+!pip install tensorflow
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import os
+import fileinput
+
+print(f"TensorFlow version = {tf.__version__}\n")
+
+SEED = 1337
+np.random.seed(SEED)
+tf.random.set_seed(SEED)
+
+CLASSES = [];
+
+for file in os.listdir("/content/"):
+    if file.endswith(".csv"):
+        CLASSES.append(os.path.splitext(file)[0])
+
+CLASSES.sort()
+
+SAMPLES_WINDOW_LEN = 1
+NUM_CLASSES = len(CLASSES)
+
+#Create a one-hot encoded matrix that is used in the output
+ONE_HOT_ENCODED_CLASSES = np.eye(NUM_CLASSES)
+
+inputs = []
+outputs = []
+
+#Read each csv file and push an input and output
+for class_index in range(NUM_CLASSES):
+  objectClass = CLASSES[class_index]
+  df = pd.read_csv("/content/" + objectClass + ".csv")
+  columns = list(df)
+  # get rid of pesky empty value lines of csv which cause NaN inputs to TensorFlow
+  df = df.dropna()
+  df = df.reset_index(drop=True)
+   
+  # calculate the number of objectClass recordings in the file
+  num_recordings = int(df.shape[0] / SAMPLES_WINDOW_LEN)
+  print(f"\u001b[32;4m{objectClass}\u001b[0m class will be output \u001b[32m{class_index}\u001b[0m of the classifier")
+  print(f"{num_recordings} samples captured for training with inputs {list(df)} \n")
+
+  # graphing
+  plt.rcParams["figure.figsize"] = (10,1)
+  pixels = np.array([df['Red'],df['Green'],df['Blue']],float)
+  pixels = np.transpose(pixels)
+  for i in range(num_recordings):
+    plt.axvline(x=i, linewidth=8, color=tuple(pixels[i]/np.max(pixels[i], axis=0)))
+  plt.show()
+  
+  #tensors
+  output = ONE_HOT_ENCODED_CLASSES[class_index]
+  for i in range(num_recordings):
+    tensor = []
+    row = []
+    for c in columns:
+      row.append(df[c][i])
+    tensor += row
+    inputs.append(tensor)
+    outputs.append(output)
+
+inputs = np.array(inputs)
+outputs = np.array(outputs)
+
+print("Data set parsing and preparation complete.")
+
+num_inputs = len(inputs)
+randomize = np.arange(num_inputs)
+np.random.shuffle(randomize)
+
+inputs = inputs[randomize]
+outputs = outputs[randomize]
+
+
+TRAIN_SPLIT = int(0.6 * num_inputs)
+TEST_SPLIT = int(0.2 * num_inputs + TRAIN_SPLIT)
+
+inputs_train, inputs_test, inputs_validate = np.split(inputs, [TRAIN_SPLIT, TEST_SPLIT])
+outputs_train, outputs_test, outputs_validate = np.split(outputs, [TRAIN_SPLIT, TEST_SPLIT])
+
+print("Data set randomization and splitting complete.")
+
+
+
+# build the model and train it
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(8, activation='relu')) 
+model.add(tf.keras.layers.Dense(5, activation='relu'))
+model.add(tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'))
+model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+history = model.fit(inputs_train, outputs_train, epochs=400, batch_size=4, validation_data=(inputs_validate, outputs_validate))
+
+
+
+
+
+#Use model to predict the test inputs
+predictions = model.predict(inputs_test)
+
+print("predictions =\n", predictions[0])
+print("actual =\n", outputs_test[0])
+
+correct_count = 0
+
+for i in range(len(predictions)):
+  label = np.argmax(predictions[i])
+  if outputs_test[i][label] == 1:
+    correct_count += 1
+
+print(correct_count)
+print(correct_count/len(outputs_test))
+
+
+#Convert the model to the TensorFlow Lite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+#Save the model to disk
+open("gesture_model.tflite", "wb").write(tflite_model)
+  
+import os
+basic_model_size = os.path.getsize("gesture_model.tflite")
+print("Model is %d bytes" % basic_model_size)
+
+
+#Convert to Arduino header file
+!echo "const unsigned char model[] = {" > /content/model.h
+!cat gesture_model.tflite | xxd -i      >> /content/model.h
+!echo "};"                              >> /content/model.h
+
+import os
+model_h_size = os.path.getsize("model.h")
+print(f"Header file, model.h, is {model_h_size:,} bytes.")
+print("\nOpen the side panel (refresh if needed). Double click model.h to download the file.")
